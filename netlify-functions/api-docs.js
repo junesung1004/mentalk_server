1229346c0f98ac8ba929fe45af4368ba5268dbc3
junesung1004 <junecoding1004@gmail.com>
2024-12-26 14:49:31 +0900

    asdasda

--- netlify-functions/api-docs.js
+++ netlify-functions/api-docs.js
@@ -1,13 +1,15 @@
 // netlify-functions/api-docs.js
-const express = require("express");
-const serverless = require("serverless-http");
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("../config/swaggerConfig"); // Swagger 설정 파일 경로
 
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("../config/swaggerConfig"); // Swagger 설정 파일
+
+// Express 앱 생성
 const app = express();
 
 // Swagger UI 서빙
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// serverless 래핑
+// serverless로 래핑
+const serverless = require("serverless-http");
 module.exports.handler = serverless(app);
